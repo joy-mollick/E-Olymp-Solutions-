4bc54d6951f55f222590865d241edd51adfee9f0
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-10-30 11:05:08 -0700

    Create Digits.cpp

---
+++ Digits.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+
+{
+    int n;
+    cin>>n;
+    if(n==0) cout<<"1"<<endl;
+
+    else{
+    int ans=log10(n);
+    cout<<ans+1<<endl;}
+    return 0;
+}
